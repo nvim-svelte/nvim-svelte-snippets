26054c50b90f9b723fa96ab433b34e6c6b352e5e
Stephen Gunn <stephengunn@gmail.com>
2025-01-21 20:02:51 -0600

    added sk- to the sveltekit snippets

--- lua/nvim-svelte-snippets/client.lua
+++ lua/nvim-svelte-snippets/client.lua
@@ -3,11 +3,11 @@ local utils = require("nvim-svelte-snippets.utils")
 local M = {}
 
 local snippets = {
-  -- Client-side load function
-  utils.create_snippet(
-    "load",
-    utils.fmt(
-      [[
+	-- Client-side load function
+	utils.create_snippet(
+		"sk-load",
+		utils.fmt(
+			[[
 export const load = satisfies PageLoad(async ({}) => {{
     {}
     return {{
@@ -15,18 +15,18 @@ export const load = satisfies PageLoad(async ({}) => {{
     }};
 }});
             ]],
-      {
-        utils.i(1, "params, fetch"),
-        utils.i(2, "// fetch your data here"),
-        utils.i(3, "prop: value"),
-      }
-    ),
-    "SvelteKit client load function"
-  ),
+			{
+				utils.i(1, "params, fetch"),
+				utils.i(2, "// fetch your data here"),
+				utils.i(3, "prop: value"),
+			}
+		),
+		"SvelteKit client load function"
+	),
 }
 
 function M.setup()
-  ls.add_snippets("typescript", snippets)
+	ls.add_snippets("typescript", snippets)
 end
 
 return M
--- lua/nvim-svelte-snippets/server.lua
+++ lua/nvim-svelte-snippets/server.lua
@@ -3,11 +3,11 @@ local utils = require("nvim-svelte-snippets.utils")
 local M = {}
 
 local snippets = {
-  -- Server-side load function
-  utils.create_snippet(
-    "sload",
-    utils.fmt(
-      [[
+	-- Server-side load function
+	utils.create_snippet(
+		"sk-load",
+		utils.fmt(
+			[[
 export const load = satisfies PageServerLoad(async ({}) => {{
     {}
     return {{
@@ -15,19 +15,19 @@ export const load = satisfies PageServerLoad(async ({}) => {{
     }};
 }});
             ]],
-      {
-        utils.i(1, "params, fetch"),
-        utils.i(2, "// fetch your data here"),
-        utils.i(3, "prop: value"),
-      }
-    ),
-    "SvelteKit server load function"
-  ),
-  -- Form actions
-  utils.create_snippet(
-    "actions",
-    utils.fmt(
-      [[
+			{
+				utils.i(1, "params, fetch"),
+				utils.i(2, "// fetch your data here"),
+				utils.i(3, "prop: value"),
+			}
+		),
+		"SvelteKit server load function"
+	),
+	-- Form actions
+	utils.create_snippet(
+		"sk-actions",
+		utils.fmt(
+			[[
 export const actions = {{
     default: async ({{ request }}) => {{
         {}
@@ -37,17 +37,17 @@ export const actions = {{
     }}
 }};
             ]],
-      {
-        utils.i(1, "const data = await request.formData();"),
-        utils.i(2, "success: true"),
-      }
-    ),
-    "SvelteKit form actions"
-  ),
+			{
+				utils.i(1, "const data = await request.formData();"),
+				utils.i(2, "success: true"),
+			}
+		),
+		"SvelteKit form actions"
+	),
 }
 
 function M.setup()
-  ls.add_snippets("typescript", snippets)
+	ls.add_snippets("typescript", snippets)
 end
 
 return M
